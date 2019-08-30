4ec3818edd11ec3f09f26dbb2b29b9a4f0c7917e
s5148464 <man.jin@griffithuni.edu.au>
2019-08-30 23:25:40 +1000

    chat and server

--- chat/my-app/src/app/app-routing.module.ts
+++ chat/my-app/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './view/login/login.component';
+import { RegComponent } from './view/reg/reg.component';
+import { MainComponent } from './view/main/main.component';
+
 
 const routes: Routes = [
-  {path: '', component:LoginComponent},
-  {path: 'home', component:HomeComponent}
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'reg', component: RegComponent },
+  { path: 'main', component: MainComponent },
+  { path: '*', redirectTo: 'login', pathMatch: 'full' }
+
 ];
 
 @NgModule({
--- chat/my-app/src/app/app.module.ts
+++ chat/my-app/src/app/app.module.ts
@@ -1,27 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { FormsModule }        from '@angular/forms';
-import { HttpClientModule }         from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_GB } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+
+// tslint:disable-next-line: jsdoc-format
+/** Config angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+import { LoginModule } from './view/login/login.module';
+import { RegModule } from './view/reg/reg.module';
+import { MainModule } from './view/main/main.module';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    // tslint:disable-next-line: jsdoc-format
+    /** include ng-zorro-antd module **/
+    NgZorroAntdModule,
+    LoginModule,
+    RegModule,
+    MainModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // tslint:disable-next-line: jsdoc-format
+  providers: [
+    { provide: NZ_I18N, useValue: en_GB }
+  ]
 })
 export class AppModule { }
