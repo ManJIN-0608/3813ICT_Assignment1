b3d092756dd71f6022d58dd8fcd76bc050b784af
s5148464 <man.jin@griffithuni.edu.au>
2019-09-01 17:56:07 +1000

    test readMe

---
+++ server/README.md
@@ -0,0 +1,8 @@
+# Summary
+The server files are largely spread out between three main files:
+server.js|data.json
+checkUser.js|fetchAllGroups.js|fetchAllChannels.js
+fetchUser.js|addGroup.js|addChannel.js
+fetchAllUsers.js|deletegroup.js|deleteChannel.js
+addUser.js|addUsersToGroup.js|addUsersToChannel.js
+fetchUser.js|deleteUsersToGroup.js|deleteUsersToChannel.js
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//Cross origin resourse sharing to cater for port 4200 to port 3000
+// Cross origin resourse sharing to cater for port 4200 to port 3000
 const cors = require('cors');
 app.use(cors());
 
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Point static path to dist if you want use your own server to serve Angular webpage
+// Point static path to dist if you want use your own server to serve Angular webpage
 app.use(express.static('http://localhost:4200'));
 
 
@@ -22,6 +22,7 @@ let server = http.listen(3000, function () {
     console.log("Server listening on: "+ host + " port: " + port);
 });
 
+// Require all routes
 require("./routes/checkUser.js")(app, path);
 require("./routes/fetchAllUsers.js")(app, path);
 require("./routes/addUser.js")(app, path);
