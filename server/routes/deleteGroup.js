4db450acee47cbc7ab065e95a27c6dc585ea7905
s5148464 <man.jin@griffithuni.edu.au>
2019-09-28 19:54:03 +1000

    delete users and groups and channels

--- server/routes/deleteGroup.js
+++ server/routes/deleteGroup.js
@@ -1,41 +1,17 @@
-const fs = require("fs");
-
 // Delete group by groupname
-module.exports = function(app, path){
+module.exports = function(app, db){
     app.post("/deleteGroup", function(req, res){
         let groupname = req.body.groupname;
-        let allData = [];
-        let groups = [];
         console.log("Made it to deleteGroup");
         console.log(groupname);
 
-        fs.readFile("./data.json", "utf8", function(err, data){
-            if(err){
-                throw err;
-            }
-
-            allData = JSON.parse(data);
-            groups = allData.groups;
-            allData.groups = groups;
-                // Removes active user from user array
-            for(let i = 0; i < groups.length; i++){
-                if(groups[i].groupname == groupname){
-                    console.log(groups[i]);
-                    groups.splice([i], 1);
-                }
-            }
-
-            let allDataJson = JSON.stringify(allData);
-                fs.writeFile("./data.json", allDataJson, "utf-8", function(err){
-                    if(err){
-                        throw err;
-                    }
-                });
-                // console.log(allData);
-                // console.log(allData.groups);
-                // console.log(allDataJson);
-                console.log(groups);
+        var myquery = {groupname:req.body.groupname};
+        let groups = db.collection('groups');
+        groups.deleteOne(myquery,(err,docs)=>{
+            groups.find({}).toArray((err, groups)=>{
                 res.send(groups);
+                console.log(groups);
+            });
         });
     });
 }
